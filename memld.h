f89de198f76900c1710220849a9194b6d373f62b
CaptBarbarosa <tandogankaan5@gmail.com>
2024-03-08 17:53:10 +0000

    End of first part

--- memld.h
+++ memld.h
@@ -1,13 +1,13 @@
 #ifndef MEMLD_H
 #define MEMLD_H
-#include<stdio.h>
-#include<stdlib.h>
+
+
 #define MAX_CHAR_SIZE 128
 
 
 typedef struct structure_database structure_database;
 typedef struct struct_field_info struct_field_info;
-typedef struct data_type_t data_type_t;
+
 
 struct structure_database{
     char name_of_the_structure[MAX_CHAR_SIZE]; //This is the name of the structure we have in our DB. We'll be able to keep track of all structures thanks to this. This is a key, if you will.
@@ -18,6 +18,18 @@ struct structure_database{
 };
 
 
+typedef enum{ // Data types in C.
+    UINT8,
+    UINT32,
+    INT32,
+    CHAR,
+    OBJ_PTR,
+    FLOAT,
+    DOUBLE,
+    OBJ_STRUCT
+} data_type_t;
+
+
 struct struct_field_info{
     char fname[MAX_CHAR_SIZE]; // Name of the field. 
     unsigned int size; //Size of a field.
@@ -25,10 +37,29 @@ struct struct_field_info{
     data_type_t dtype;
     char nested_str_name[MAX_CHAR_SIZE];
 };
-
-
-struct 
-
-
-
 #endif
+
+
+
+/*
+Assume that we have the following structure.
+typedef struct Student{
+    char std_name[30];
+    unsigned int entry_year;
+    unsigned int age;
+    struct Student *student_advisor;
+    float student_budget;
+}student;
+
+Our "structure_database" will be storing the following.
+name_of_the_structure = student
+ds_size = sizeof(student)
+n_fields = 5
+fields
+next = NULL
+
+In the fields[0] we will store the "std_name", sizeof(char)*30, offset=0, CHAR, NULL
+...
+...
+In the fields[3] student_advisor, sizeof(void *), 38, OBJ_PTR, student will be kept.
+*/
