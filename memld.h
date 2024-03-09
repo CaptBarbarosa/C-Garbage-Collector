3805c61415775e4d053ee8c85eef3c38452966c6
CaptBarbarosa <tandogankaan5@gmail.com>
2024-03-09 18:27:01 +0000

    21:27

--- memld.h
+++ memld.h
@@ -9,15 +9,6 @@ typedef struct structure_database structure_database;
 typedef struct struct_field_info struct_field_info;
 
 
-struct structure_database{
-    char name_of_the_structure[MAX_CHAR_SIZE]; //This is the name of the structure we have in our DB. We'll be able to keep track of all structures thanks to this. This is a key, if you will.
-    unsigned int ds_size; // What is the size of the field. 
-    unsigned int n_fields; // How many fields does the structure has.
-    struct_field_info *field; //Array of fields. These will keep the information about every field within a structure. For example lets say structure Father has a field char *middle_name; This part of our code will keep track of it.  
-    structure_database *next; // Points to the next. 
-};
-
-
 typedef enum{ // Data types in C.
     UINT8,
     UINT32,
@@ -30,16 +21,28 @@ typedef enum{ // Data types in C.
 } data_type_t;
 
 
-struct struct_field_info{
+struct struct_field_info{ // This is the information about the fields( or variables) of structure
     char fname[MAX_CHAR_SIZE]; // Name of the field. 
     unsigned int size; //Size of a field.
     unsigned int offset; // Displacement of the field from the beginning of the structure
     data_type_t dtype;
     char nested_str_name[MAX_CHAR_SIZE];
 };
+
+
+struct structure_database{
+    char name_of_the_structure[MAX_CHAR_SIZE]; //This is the name of the structure we have in our DB. We'll be able to keep track of all structures thanks to this. This is a key, if you will.
+    unsigned int ds_size; // What is the size of the field. 
+    unsigned int n_fields; // How many fields does the structure has.
+    struct_field_info *field; //Array of fields. These will keep the information about every field within a structure. For example lets say structure Father has a field char *middle_name; This part of our code will keep track of it.  
+    structure_database *next; // Points to the next. 
+};
+
+typedef struct db_head_{
+    struct structure_database *head;
+    int count;
+}db_head;
 #endif
-
-
 
 /*
 Assume that we have the following structure.
@@ -58,6 +61,7 @@ n_fields = 5
 fields
 next = NULL
 
+---> The fields of the structure_database will keep the information regarding the variables in that structure.
 In the fields[0] we will store the "std_name", sizeof(char)*30, offset=0, CHAR, NULL
 ...
 ...
