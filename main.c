1f57f6b7f95f82aa382a0b3efd920c91706daf55
CaptBarbarosa <tandogankaan5@gmail.com>
2024-12-02 13:09:43 +0200

    deneme

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stddef.h>
 #include"memld.h"
 /*
 We will be putting a Memory Leak Detector library between the C application abd Kernel. By doing so we'll keep track of the all memory objects used by the application.
@@ -10,5 +11,6 @@ We will be putting a Memory Leak Detector library between the C application abd 
 -> And lastly our tools detect any memory leaks 
 */
 int main(){
+    // In the udemy courses I follow it said that I should have defined the names of the structure variables here but that seems like not very user friendly to me. I asked how could I dynamically extract the user names from the code and chatgpt told me to try "reflection" or "metaprogramming". So what I'm gonna try is that I'll try to dynamically extract the file names by that methods. But the thing is I don't know how. So, I'll stop this project for now and try to get back to it in a week or in couple of weeks. 12.03.2023
     return 0;
 }
